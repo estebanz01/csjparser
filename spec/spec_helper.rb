3c097ece1aa6ce257e37c5261024ceb141270d85
esteban zapata <ezapata@altavistaed.com>
2016-06-28 08:41:07 -0500

    coverage: Setup coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'csjparser'
 require 'rspec'
+require 'coveralls'
 
+# Setup coveralls.
+Coveralls.wear!
 
 RSpec.configure do |config|
 end
